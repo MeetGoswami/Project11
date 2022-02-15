bcd72e19d83c0f8cda023afe3cfb632695f3e715
MeetGoswami <97733980+MeetGoswami@users.noreply.github.com>
2022-02-15 10:41:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld
 	{
 		System.out.println("Hello World");
 		//System.out.println("This is a change");
-		//This is a new comment
 		System.out.println("This is a change after adding webhook");
 		System.exit(0);
 	}
